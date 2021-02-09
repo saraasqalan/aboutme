32330b50c0d1c63457c75bd98530719521e2982e
saraasqalan <asqalansara@gmail.com>
2021-02-09 13:28:27 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -6,41 +6,51 @@ console.log("please answer by yes or no")
 alert("please answer by yes or no");
 
 let userPlace = prompt('Did you live in Amman');
-if(userPlace.toLocaleLowerCase()=='yes')
+switch(userPlace.toLowerCase()){
+case ('yes'):
 alert('Your delevary is free');
-else if(userPlace.toLocaleLowerCase()=='no')
- alert ('Your delevary is 3 JD');
+case ('y'):
+alert('Your delevary is free');
+break;
+case('no'):
+alert ('Your delevary is 3 JD');
+case('n'):
+alert ('Your delevary is 3 JD');
+break;
+default:
+  alert('IT\'S NOT WORKING')
+}
 
  console.log("please answer by yes or no");
 
  let gender = prompt('Are you Male');
-if(gender.toLowerCase() === 'yes'){
+if(gender.toLowerCase() === 'yes' || 'Y'){
   alert('Your course is 25% discount!');
-}else if(gender.toLowerCase() === 'no'){
+}else if(gender.toLowerCase() === 'no' || 'n'){
   alert('you got  50% discount!');
 
   console.log("please answer by yes or no");
 
   let userAge = prompt('is your age between 18-28')
-if(userAge.toLocaleLowerCase()=='yes')
+if(userAge.toLocaleLowerCase()=='yes' || 'Y')
 alert('Your next course is free');
-else if(userAge.toLocaleLowerCase()=='no')
+else if(userAge.toLocaleLowerCase()=='no' || 'n')
  alert ('you have 50% discount in next course');
 
  console.log("please answer by yes or no");
 
  let userNationalitye = prompt('are you jordanian')
- if(userNationalitye.toLocaleLowerCase()=='yes')
+ if(userNationalitye.toLocaleLowerCase()=='yes' || 'Y')
  alert('you have 30% discount in bus fees');
- else if(ususerNationality.toLocaleLowerCase()=='no')
+ else if(ususerNationality.toLocaleLowerCase()=='no' || 'n')
   alert ('you have 50% discount in bus fees');
 
   console.log("please answer by yes or no");
 
   let courseName =prompt('are you intrest in css')
-  if(courseName.toLocaleLowerCase()=='yes')
+  if(courseName.toLocaleLowerCase()=='yes' || 'Y')
   alert('Your css course is free');
-  else if(courseName.toLocaleLowerCase()=='no')
+  else if(courseName.toLocaleLowerCase()=='no' || 'n')
    alert ('you have 50% discount in anycourse');
 
 
